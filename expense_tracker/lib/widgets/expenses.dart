1a021c8106e391a4c07954ea7cee4770fc6a19f2
Supavadee Phusanam <supavadee.phusanam@ibm.com>
2024-10-25 14:54:08 +0700

    Apply theme

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
--- expense_tracker/lib/widgets/expenses_list/expense_item.dart
+++ expense_tracker/lib/widgets/expenses_list/expense_item.dart
@@ -10,10 +10,17 @@ class ExpenseItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
             const SizedBox(height: 4),
             Row(
               children: [
