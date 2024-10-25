d31aa5e968637070ca04f4f938609a8aa35bfafd
Supavadee Phusanam <supavadee.phusanam@ibm.com>
2024-10-25 11:26:37 +0700

    Full screen modal

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -64,6 +64,8 @@ class _NewExpenseState extends State<NewExpense> {
           date: _selectedDate!,
           category: _selecedCategoty),
     );
+
+    Navigator.pop(context);
   }
 
   @override
@@ -77,7 +79,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
