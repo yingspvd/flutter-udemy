4947ef0554ce49c13027cf5a3fe25d968ddc0665
Supavadee Phusanam <supavadee.phusanam@ibm.com>
2024-10-25 10:54:25 +0700

    Create expense tracker and new expense page

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    home: ,
-  ))
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Expenses(),
+    ),
+  );
 }
