19f2192181edc941306f668b46fe6ece5b6af753
Supavadee Phusanam <supavadee.phusanam@ibm.com>
2024-10-29 16:31:51 +0700

    add BottomNavigationBar

--- meals_app/lib/screens/meals.dart
+++ meals_app/lib/screens/meals.dart
@@ -6,11 +6,11 @@ import 'package:meals_app/widgets/meal_item.dart';
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
+    this.title,
     required this.meals,
   });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -55,9 +55,13 @@ class MealsScreen extends StatelessWidget {
               ));
     }
 
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text(title!),
         ),
         body: content);
   }
