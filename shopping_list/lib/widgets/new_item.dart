92131192266ea1253c74b3fab9f4c04d1fd839cb
Supavadee Phusanam <supavadee.phusanam@ibm.com>
2024-11-01 14:52:57 +0700

    Send Post request to Firebase

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -21,14 +24,18 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
-        ),
-      );
+      final url = Uri.https('flutter-prep-80bfd-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+      http.post(url,
+          headers: {
+            'Content-Type': 'appication/json',
+          },
+          body: json.encode({
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          }));
+      Navigator.of(context).pop();
     }
   }
 
