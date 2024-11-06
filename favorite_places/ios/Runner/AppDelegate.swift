6482125f5451406e77a86dcf387083ded4117c06
Supavadee Phusanam <supavadee.phusanam@ibm.com>
2024-11-06 17:16:49 +0700

    open google map

--- favorite_places/ios/Runner/AppDelegate.swift
+++ favorite_places/ios/Runner/AppDelegate.swift
@@ -1,12 +1,14 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCpniq0XRBe1cg1KX5pzyv_jwxp9UnzEZ4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
