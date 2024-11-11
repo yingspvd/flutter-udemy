74e6540c293b1f69f9c941c288a2836cada9a141
Supavadee Phusanam <supavadee.phusanam@ibm.com>
2024-11-11 17:15:09 +0700

    push notifications topic

--- chat_app/lib/screen/chat.dart
+++ chat_app/lib/screen/chat.dart
@@ -1,11 +1,32 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() {
+    return _ChatScreenState();
+  }
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setUpPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setUpPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
