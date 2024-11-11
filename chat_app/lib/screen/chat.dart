445b834320e540a34662f699dcb270e0048106a2
Supavadee Phusanam <supavadee.phusanam@ibm.com>
2024-11-11 15:32:08 +0700

    send and read message to-from firestore

--- chat_app/lib/screen/chat.dart
+++ chat_app/lib/screen/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -8,23 +10,27 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Chat'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: Icon(
-              Icons.exit_to_app,
-              color: Theme.of(context).colorScheme.primary,
+        appBar: AppBar(
+          title: const Text('Flutter Chat'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            )
+          ],
+        ),
+        body: const Column(
+          children: [
+            Expanded(
+              child: ChatMessages(),
             ),
-          )
-        ],
-      ),
-      body: const Center(
-        child: Text('Logged in!'),
-      ),
-    );
+            NewMessage(),
+          ],
+        ));
   }
 }
